5014987f95784d54042aab04ca12c90303bcdc1b
LepkoQQ <lepko.san@gmail.com>
2025-01-21 17:09:57 +0100

    upgrade local postgres to 15, fix import script

--- import_db.sh
+++ import_db.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # EDIT DATABASE NAME TO CHOOSE WHICH ONE YOU WANT
-DATABASE_NAME="pravnamreza"
+DATABASE_NAME="pravna_mreza"
+K8S_NAMESPACE="pravnamreza"
+SECRETS_NAME="pmvd-config"
 
 # DATABASE PASSWORD IS DYNAMICALLY RETRIEVED FROM THE CLUSTER
-DATABASE_PASSWORD=$(kubectl get secret postgresql -n shared -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+DATABASE_USER=$(kubectl get secret $SECRETS_NAME -n $K8S_NAMESPACE -o jsonpath="{.data.DB_USERNAME}" | base64 --decode)
+DATABASE_PASSWORD=$(kubectl get secret $SECRETS_NAME -n $K8S_NAMESPACE -o jsonpath="{.data.DB_PASSWORD}" | base64 --decode)
 
 echo
 echo "PORT FORWARDING"
-nohup kubectl port-forward pod/postgresql-11-postgresql-0 54321:5432 --namespace=shared &>/dev/null &
+nohup kubectl port-forward pod/postgresql-15-0 54321:5432 --namespace=shared &>/dev/null &
 
 # store the kubectl pid for later
 KUBECTL_PID=$!
@@ -19,28 +22,29 @@ sleep 5
 echo
 echo "DUMPING DATABASE TO db.dump"
 PGPASSWORD=$DATABASE_PASSWORD \
-    pg_dump -U postgres \
+    pg_dump -U $DATABASE_USER \
         -f db.dump \
         -d "$DATABASE_NAME" \
         -p 54321 \
-        -h localhost \
-        --no-owner
+        -h localhost
 
-echo
-echo "DROPPING THE DB VOLUME"
-sudo docker compose down -v
-sudo docker compose up -d
+# echo
+# echo "DROPPING THE DB VOLUME"
+# docker-compose down -v
+# docker-compose up -d
 
-sleep 5
+# sleep 5
 
-echo
-echo "LOADING DB INTO CONTAINER"
-sudo docker container exec -i $(sudo docker compose ps -q db) psql -U wagtail wagtail < db.dump
+# echo
+# echo "LOADING DB INTO CONTAINER"
+# docker container exec -i $(docker-compose ps -q db) psql -U wagtail wagtail < db.dump
 
-sudo docker-compose down
+# docker-compose down
 
 echo "STOPPING PORT FORWARDING"
 kill $KUBECTL_PID
 
 echo
-echo "ALL DONE, YOU CAN RUN docker-compose up AND/OR DELETE db.dump"
+echo "ALL DONE, DATABASE DUMP SAVED to db.dump"
+echo "DROPPING AND IMPORTING DB IS DISABLED, DO IT MANUALLY OR FIX THE SCRIPT :D"
+# echo "ALL DONE, YOU CAN RUN docker-compose up AND/OR DELETE db.dump"
