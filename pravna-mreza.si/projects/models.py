0f386a1da343f33ebbc6541d12137f4ba2dd4a95
LepkoQQ <lepko.san@gmail.com>
2025-06-16 21:16:54 +0200

    project list page

--- pravna-mreza.si/projects/models.py
+++ pravna-mreza.si/projects/models.py
@@ -5,12 +5,6 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
 
-# class ProjectTag(models.Model):
-#     name = models.TextField()
-
-#     def __str__(self):
-#         return self.name
-
 
 class ProjectsBlock(blocks.StructBlock):
     project = blocks.PageChooserBlock(page_type="projects.ProjectPage")
@@ -20,9 +14,7 @@ class ProjectsBlock(blocks.StructBlock):
 
 
 class ProjectPage(Page):
-    # date = models.DateField()
     preview_text = RichTextField(blank=False, null=False, default="")
-    # tag = models.ForeignKey(ProjectTag, on_delete=models.SET_NULL, null=True, blank=True)
     preview_image = models.ForeignKey(
         "wagtailimages.Image",
         null=True,
@@ -47,8 +39,6 @@ class ProjectPage(Page):
     )
 
     content_panels = Page.content_panels + [
-        # FieldPanel('date'),
-        # FieldPanel('tag'),
         FieldPanel("preview_text", classname="full"),
         FieldPanel("preview_image"),
         FieldPanel("intro_text"),
@@ -72,8 +62,6 @@ class ProjectPage(Page):
 
 
 class ProjectsArchivePage(Page):
-    headline_first = models.TextField(verbose_name="Naslovnica prvi del", blank=True)
-    headline_second = models.TextField(verbose_name="Naslovnica drugi del", blank=True)
     headline_image = models.ForeignKey(
         "wagtailimages.Image",
         null=True,
@@ -82,33 +70,45 @@ class ProjectsArchivePage(Page):
         related_name="+",
         verbose_name="Slika na naslovnici",
     )
+    projects_title = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="Naslov aktualnih projektov",
+    )
     projects = StreamField(
         [
             ("project", ProjectsBlock()),
         ],
         null=True,
         blank=True,
-        use_json_field=True,
+        verbose_name="Aktualni projekti",
+    )
+    archived_projects_title = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="Naslov arhiviranih projektov",
+    )
+    archived_projects = StreamField(
+        [
+            ("project", ProjectsBlock()),
+        ],
+        null=True,
+        blank=True,
+        verbose_name="Arhivirani projekti",
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("headline_first"),
-        FieldPanel("headline_second"),
         FieldPanel("headline_image"),
+        FieldPanel("projects_title"),
         FieldPanel("projects"),
+        FieldPanel("archived_projects_title"),
+        FieldPanel("archived_projects"),
     ]
 
     parent_page_types = ["home.HomePage"]
     subpage_types = ["home.GenericPage", "ProjectPage"]
-
-    # def get_context(self, request):
-    #     # Update context to include only published posts, ordered by reverse-chron
-    #     context = super().get_context(request)
-    #     # all_projects = ProjectPage.objects.all().live().order_by("-first_published_at")
-    #     all_projects = self.projects
-    #     paginator = Paginator(all_projects, 10)
-    #     context["projects"] = paginator.get_page(request.GET.get("page"))
-    #     return context
 
     class Meta:
         verbose_name = "Seznam projektov"
